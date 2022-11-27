79bad437a75390499b28fd739395334fd444fb90
임요한 <dladygks506@gmail.com>
2022-11-28 00:36:20 +0900

    [013-1-CART_IMPROVEMENT] 장바구니 기능 개선

--- order_api/src/main/java/com/zerobase/cms/order_api/application/CartApplication.java
+++ order_api/src/main/java/com/zerobase/cms/order_api/application/CartApplication.java
@@ -10,11 +10,12 @@ import com.zerobase.cms.order_api.service.ProductSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.zerobase.cms.order_api.exception.ErrorCode.ITEM_ENOUGH;
-import static com.zerobase.cms.order_api.exception.ErrorCode.NOT_FOUND_PRODUCT;
+import static com.zerobase.cms.order_api.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +26,11 @@ public class CartApplication {
 
     public Cart addCart(Long customerId, AddProductCartForm form) {
         Product product = productSearchService.getProduct(form.getId());
+
+        // redis 서버에서 customerId(PK)에 해당하는 Cart가 있는지 조회한다.
         Cart cart = cartService.getCart(customerId);
+
+        // redis 서버에 있는 Cart에 아이템을  새로 만드
         if (cart != null && !addAble(cart, product, form)) {
             throw new CustomException(ITEM_ENOUGH);
         }
@@ -34,23 +39,40 @@ public class CartApplication {
     }
 
     private boolean addAble(Cart cart, Product product, AddProductCartForm form) {
-        Cart.Product cartProduct = cart.getProducts().stream().filter(p -> p.getId().equals(form.getId()))
-                .findFirst().orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
+//        Cart.Product cartProduct =
+
+        // Cart에 해당 Product가 이미 담겨있는지 확인
+        Optional<Cart.Product> optionalProduct = cart.getProducts().stream().filter(p -> p.getId().equals(form.getId()))
+                .findFirst();
+
+
+        Map<Long, Integer> cartItemCountMap = new HashMap<>();
 
         // Map으로 하는 이유는 속도 때문!
-        // 현재 카트에 담아놓은 각 아이템의 수량
-        Map<Long, Integer> cartItemCountMap = cartProduct.getItems().stream()
-                .collect(Collectors.toMap(Cart.ProductItem::getId, Cart.ProductItem::getCount));
+        // Cart에 해당 Product가 이미 담겨있는 경우, Product의 수량을 Map으료 표시함
+        if (optionalProduct.isPresent()) {
+            Cart.Product cartProduct = optionalProduct.get();
+            // 현재 카트에 담아놓은 각 아이템의 수량
+            cartItemCountMap = cartProduct.getItems().stream()
+                    .collect(Collectors.toMap(Cart.ProductItem::getId, Cart.ProductItem::getCount));
+        }
 
         // 각 아이템의 현재 남은 재고
         Map<Long, Integer> currentItemCountMap = product.getProductItemList().stream()
                 .collect(Collectors.toMap(ProductItem::getId, ProductItem::getCount));
 
+        // 람다식에 넣기 위해 변수 재정의
+        Map<Long, Integer> finalCartItemCountMap = cartItemCountMap;
+
         // [현재 카트에 담아놓은 각 아이템의 수량 + 새로 카트에 담은 아이템 수량 >= 현재 아이템 재고] 체크
         return form.getItems().stream()
                 .noneMatch(formItem -> {
-                    Integer cartCount = cartItemCountMap.get(formItem.getId());
+                    // 카트에 해당 아이템이 없는 경우 Cart에 담긴 해당 아이템의 수량을 0으로 표기.
+                    Integer cartCount = finalCartItemCountMap.getOrDefault(formItem.getId(), 0);
                     Integer currentCount = currentItemCountMap.get(formItem.getId());
+                    if (currentCount == null) {
+                        throw new CustomException(NOT_FOUND_ITEM);
+                    }
                     return formItem.getCount() + cartCount >= currentCount;
                 });
     }
--- order_api/src/main/java/com/zerobase/cms/order_api/service/CartService.java
+++ order_api/src/main/java/com/zerobase/cms/order_api/service/CartService.java
@@ -20,6 +20,7 @@ import java.util.stream.Collectors;
 public class CartService {
 
     private final RedisClient redisClient;
+    private int totalPrice = 0;
 
     public Cart getCart(Long customerId) {
         return redisClient.get(customerId, Cart.class);
@@ -28,7 +29,10 @@ public class CartService {
     @Transactional
     public Cart addCart(Long customerId, AddProductCartForm form) {
 
+        // redis 서버로부터 PK(customerId)에 해당하는 Cart 데이터를 가져온다.
         Cart cart = redisClient.get(customerId, Cart.class);
+
+        // redis 서버에 고객 소유의 Cart가 없으면 새로 객체를 생성해준다. (1인 최대 1 Cart)
         if (cart == null) {
             cart = new Cart();
             cart.setCustomerId(customerId);
@@ -38,40 +42,61 @@ public class CartService {
                 .filter(p -> p.getId().equals(form.getId()))
                 .findFirst();
 
+        // redis에 고객 소유의 Cart가 이미 존재하면서, 새로 Cart에 추가한 아이템이 이미 Cart에 담겨 있는 경우
         // 이전에 같은 Product 장바구니에 담은 적이 있다면 장바구니 내 해당 Product에 생긴 변화를 감지한다.
         if (optionalProduct.isPresent()) {
             Cart.Product redisProduct = optionalProduct.get();
 
-            // 장바구니에 새로 담는 것을 요청받은 ProductItem 목록
+            // 장바구니에 담는 것을 요청받은 ProductItem 목록
             List<Cart.ProductItem> items = form.getItems().stream().map(Cart.ProductItem::from).collect(Collectors.toList());
 
-            // 해당 클라이언트가 기존 장바구니에 담고있던 Product의 ProductItem 목록
+            // redis에 저장된 해당 클라이언트의 기존 장바구니에 담겨있던 ProductItem 목록
             Map<Long, Cart.ProductItem> redisItemMap = redisProduct.getItems().stream()
                     .collect(Collectors.toMap(Cart.ProductItem::getId, item -> item));
 
             // 장바구니 내 담긴 Product와 새로 담은 Product의 id는 일치한데 이름이 바꼈다면
             if (!redisProduct.getName().equals(form.getName())) {
-                cart.addMessage(redisProduct.getName() + "의 정보가 변경되었습니다. 확인 부탁드립니다.");
+                cart.addMessage(redisProduct.getName() + "의 정보가 " + form.getName() + "으로 변경되었습니다. 확인 부탁드립니다.");
+                redisProduct.setName(form.getName());
             }
 
+            /**
+             * cart -> redisProduct -> redisItemMap -> redisItem 순서로 결국 redis에서 뽑아낸것이기 떄문에 set을 통해 값 변경한게
+             * db에 반영된다.
+             */
             for (Cart.ProductItem item : items) {
                 Cart.ProductItem redisItem = redisItemMap.get(item.getId());
                 if (redisItem == null) {
-                    // happy case - 기존 장바구니에 있는 것과 새로 담은 ProductItem이 겹치지 않는다면 추가로 담아준다.
+                    // happy case - 기존 장바구니에 있는 것과 새로 담은 ProductItem이 겹치지 않는 경우
                     redisProduct.getItems().add(item);
+
                 } else { // 기존 장바구니에 있는 ProductItem과 새로 담은 것은 아이템이 같을 경우
                     // 가격이 다르다면
                     if (!redisItem.getPrice().equals(item.getPrice())) {
-                        cart.addMessage(redisProduct.getName() + item.getName() + "의 가격이 변경되었습니다.");
+                        cart.addMessage(item.getName() + "의 가격이 " + item.getPrice() + "으로 변경되었습니다.");
+                        redisItem.setPrice(item.getPrice());
                     }
+                    // 장바구니에 담긴 아이템 수량 증가
                     redisItem.setCount(redisItem.getCount() + item.getCount());
+                }
+                List<Cart.Product> products = cart.getProducts();
+                for (Cart.Product product : products) {
+                    product.getItems().forEach(x -> {
+                        totalPrice += x.getPrice() * x.getCount();
+                    });
                 }
             }
         } else {
-            // 이전에 같은 상품을 장바구니에 담은적이 없다면 장바구니에 해당 상품 추가
+            // 고객 소유의 Cart가 redis서버에 없거나 이전에 같은 상품을 장바구니에 담은적이 없는 경우
             Cart.Product product = Cart.Product.from(form);
             cart.getProducts().add(product);
+            form.getItems().forEach(x -> {
+                totalPrice += x.getPrice() * x.getCount();
+            });
         }
+
+        cart.setTotalPrice(totalPrice);
+        // redis 서버에 Cart 데이터 반영
         redisClient.put(customerId, cart);
         return cart;
     }
